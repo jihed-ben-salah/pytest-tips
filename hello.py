76aeeb0c1bd68faf415a434a3572f1c7bd9aa381
Jihed Ben Salah <61364167+jihed-ben-salah@users.noreply.github.com>
2023-09-27 13:37:28 +0000

    adding initial layout for python project

---
+++ hello.py
@@ -0,0 +1,7 @@
+def write_hello():
+    return "hello"
+
+
+# this is nonsens code
+jihed = "jihed"
+jihed = jihed
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import write_hello
+
+
+def test_hello():
+    assert "hello" == write_hello()
